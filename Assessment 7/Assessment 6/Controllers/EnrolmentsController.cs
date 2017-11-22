9e5203e6a1b281b628d6b9ebb90c9eb5a7a70078
Mousheen <mousheen_6@hotmail.com>
2017-11-23 12:42:33 +1300

    Assessment 7
    ASP.NET MVC

---
+++ Assessment 7/Assessment 6/Controllers/EnrolmentsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Assessment_6;
+
+namespace Assessment_6.Controllers
+{
+    public class EnrolmentsController : Controller
+    {
+        private visionDatabaseEntities db = new visionDatabaseEntities();
+
+        // GET: Enrolments
+        public ActionResult Index()
+        {
+            var enrolments = db.Enrolments.Include(e => e.Student);
+            return View(enrolments.ToList());
+        }
+
+        // GET: Enrolments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrolment enrolment = db.Enrolments.Find(id);
+            if (enrolment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrolment);
+        }
+
+        // GET: Enrolments/Create
+        public ActionResult Create()
+        {
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "FirstName");
+            return View();
+        }
+
+        // POST: Enrolments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EnrolmentID,Grade,EnrolmentComplete,StudentID")] Enrolment enrolment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Enrolments.Add(enrolment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "FirstName", enrolment.StudentID);
+            return View(enrolment);
+        }
+
+        // GET: Enrolments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrolment enrolment = db.Enrolments.Find(id);
+            if (enrolment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "FirstName", enrolment.StudentID);
+            return View(enrolment);
+        }
+
+        // POST: Enrolments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EnrolmentID,Grade,EnrolmentComplete,StudentID")] Enrolment enrolment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(enrolment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "FirstName", enrolment.StudentID);
+            return View(enrolment);
+        }
+
+        // GET: Enrolments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrolment enrolment = db.Enrolments.Find(id);
+            if (enrolment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrolment);
+        }
+
+        // POST: Enrolments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Enrolment enrolment = db.Enrolments.Find(id);
+            db.Enrolments.Remove(enrolment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Assessment 7/Assessment 6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assessment_6.MvcApplication" Language="C#" %>
